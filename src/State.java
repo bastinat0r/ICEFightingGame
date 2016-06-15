064cd53cfb8d05d4aad8349482c0d0060de10f4c
Sebastian Mai <bastinat0r@bastinat0r.de>
2016-06-15 13:33:51 +0200

    adding classes for state and testai

---
+++ src/State.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author sebastian
+ *
+ */
+public class State {
+	public boolean hitbox1; /*a*/
+	public boolean hitbox2; /*b*/
+	public boolean hitbox3; /*ra*/
+	public boolean hitbox4; /*la*/
+	public boolean hitbox5; /*da*/
+	public boolean hitbox6; /*rb*/
+	public boolean hitbox7; /*lb*/
+	public boolean hitbox8; /*db*/
+	public boolean close_to_enemy;
+	public boolean far_from_enemy;
+	public boolean wall_front;
+	public boolean wall_back;
+	public int enemy_state; /* stehen, vor, zurück, fern, nahkampf */
+	
+	/*dateiname - unser char+ gegener char*/
+	/*additional: values for each action*/
+	/*visits*/
+	
+}
---
+++ src/TestAI.java
@@ -0,0 +1,90 @@
+import gameInterface.AIInterface;
+import structs.FrameData;
+import structs.GameData;
+import structs.Key;
+import java.util.Random;
+
+
+public class TestAI implements AIInterface {
+	public Random rnd;
+	private static Key[] action_keymap;
+	
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getCharacter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void getInformation(FrameData arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int initialize(GameData arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+		rnd = new Random();
+		action_keymap = new Key[20];
+		for(int i = 0; i<action_keymap.length; i++){
+			action_keymap[i] = new Key();
+			if(i%5 == 0 || i%5 == 1) {
+				action_keymap[i].L = true;
+			}
+			if(i%5 == 1 || i%5 == 2 || i%5 == 3){
+				action_keymap[i].D = true;
+			}
+			if(i%5 == 3 || i%5 == 4) {
+				action_keymap[i].R = true;
+			}
+			if(i >=5 && i < 10) {
+				action_keymap[i].A = true;
+			}
+			if(i >=10 && i < 15) {
+				action_keymap[i].B = true;
+			}
+			if(i >=15 && i < 20) {
+				action_keymap[i].C = true;
+			}
+		}
+		/*
+		action_keymap[0].L = true;
+		action_keymap[1].L = true;
+		action_keymap[1].D = true;
+		action_keymap[2].D = true;
+		action_keymap[3].R = true;
+		action_keymap[3].D = true;
+		action_keymap[4].R = true;
+		*/
+				
+		return 0;
+	}
+	private int chose_action_from_state() {
+		float[] testvalues = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5.5f};
+		float value_offset = 10f;
+		return rnd.nextInt(20);
+	}
+
+	@Override
+	public Key input() {
+		// every key is set randomly.
+		int x = chose_action_from_state();
+
+
+		return action_keymap[x];
+	}
+
+	@Override
+	public void processing() {
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
